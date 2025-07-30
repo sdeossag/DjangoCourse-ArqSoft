aa24ca384a71a6b2bddbf781d5abb36e5aa24e4a
sdeossag <sdeossag@eafit.edu.co>
2025-07-29 21:13:29 -0500

    Segundo commit

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def homePageView(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+from django.shortcuts import render # here by default
+from django.http import HttpResponse # new
+from django.views.generic import TemplateView
+ # Create your views here.
+def homePageView(request): # new
+    return HttpResponse('Hello World!') # new
+class HomePageView(TemplateView):
+    template_name = 'home.html'
